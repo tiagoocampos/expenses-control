f80f43480eb1046cca464bdcaffb4f59ae769b9c
Tiago Campos <droiddzn68@gmail.com>
2026-08-27 22:21:12 -0300

    fix backend deploy: authmodule not imported in categorymodule

--- backend-nest/src/categories/categories.module.ts
+++ backend-nest/src/categories/categories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
